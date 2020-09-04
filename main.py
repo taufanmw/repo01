c76afd02f9e2fc0096798ca8b313e5a8d0089045
topan <taufanmuhamadwijaya@gmail.com>
2020-09-04 10:58:45 +0700

    file baru 1

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+print('hello world')
+try :
+    r= requests.get('http://www.google.com')
+        print(r.status_code)
+    if r.status_code ==200:
+      print(r.text)
+except Exception as e:
+      print('Ada Error' + e)
