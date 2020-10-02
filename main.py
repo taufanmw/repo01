12ff2091a6886cd8aa0a0d41f336a3e2f7ae0372
topan <taufanmuhamadwijaya@gmail.com>
2020-10-02 13:47:22 +0700

    file baru 1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 
 print('hello world')
 try :
-    r= requests.get('http://www.google.com')
-        print(r.status_code)
+    r = requests.get('https://www.google.com')
+       print(r.status_code)
     if r.status_code ==200:
       print(r.text)
-except Exception as e:
+except Exception as e;
       print('Ada Error' + e)
